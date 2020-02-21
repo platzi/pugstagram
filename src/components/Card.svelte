8f6f857a001b191e5863d72278a862cfc4f07aea
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-21 12:19:23 -0500

    Lock S-foils in attack position

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,5 +1,9 @@
 <script>
   import Comments from "./Comments.svelte";
+  import Modal from "./Modal.svelte";
+  import Share from "./Share.svelte";
+
+  import { blur } from "svelte/transition";
 
   export let username;
   export let location;
@@ -7,6 +11,12 @@
   export let postComment;
   export let comments;
   export let avatar;
+
+  let isModal = false;
+
+  function handleClick() {
+    isModal = !isModal;
+  }
 </script>
 
 <style>
@@ -123,6 +133,13 @@
 </style>
 
 <div class="Card">
+  {#if isModal}
+    <div transition:blur>
+      <Modal>
+        <Share on:click={handleClick} />
+      </Modal>
+    </div>
+  {/if}
   <div class="Card-container">
     <div class="Card-Header">
       <div class="Card-user">
@@ -144,7 +161,7 @@
     <div class="Card-icons">
       <div class="Card-icons-firts">
         <i class="fas fa-heart" />
-        <i class="fas fa-paper-plane" />
+        <i class="fas fa-paper-plane" on:click={handleClick} />
       </div>
       <div class="Card-icons-second">
         <i class="fas fa-bookmark" />
--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -31,7 +31,7 @@
 <div class="Profile">
   <div class="Profile-content">
     <div class="Profile-avatar">
-      <img src="https//arepa.s3.amazonaws.com/oscar.png" alt="" />
+      <img src="https//arepa.s3.amazonaws.com/oscar.jpg" alt="" />
     </div>
     <div class="Profile-info">
       <h2>{nickname}</h2>
